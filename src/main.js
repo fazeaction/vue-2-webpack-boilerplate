40c3d80c9a210d7e48639ebd9d40b4a18093434a
miguelangel <miguelangel@vasava.es>
2017-06-13 13:20:01 +0200

    bam!

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import { sync } from 'vuex-router-sync'
 import store from './data/vuex/store'
+
 
 import routes from './config/routes'
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({ routes })
+
+sync(store, router)
 
 /* eslint-disable no-new */
 new Vue({ router, store }).$mount('#app')
